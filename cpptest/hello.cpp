ce6aff1bd25aee4cab6e09a69b9ba0d63ee47779
“autohe” <he.bin@hotmail.com>
2019-02-09 12:07:22 +0800

    hehe v1.0

--- cpptest/hello.cpp
+++ cpptest/hello.cpp
@@ -6,5 +6,6 @@ int main()
     cout << "Come up and C++ me some time";
     cout << endl;
     cout << "You won't regret it!" << endl;
+    cout << "This is the upgrated version v 1.0!" << endl;
     return 0;
 }
